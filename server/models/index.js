d3a52a94f009489229093f5f313d66cce0bdb354
Facundo Victor <facundovt@gmail.com>
2017-01-14 23:56:50 -0300

    Add support for model associations

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,8 @@
 /*
  * Author : Facundo Victor <facundovt@gmail.com>
  *
- * This connectes Sequelize to the database, finds every sequelize model in the
- * current directory and it loads it.
+ * This module connectes Sequelize to the database, finds every sequelize model
+ * in the current directory and it loads it.
  */
 
  'use strict';
@@ -16,10 +16,24 @@ const Sequelize = require('sequelize');
 
 
 let db = {
-  initialize : function (conf) {
-    let me = this;
 
-    me.sequelize = new Sequelize(conf.database, conf.username, conf.password, {
+  /*
+   * Creates a new instance of Sequelize connected to the database. Finds every
+   * sequelize model in the current directory (models), and it loads it. And
+   * finally, it loads all model associations.
+   *
+   * References: http://docs.sequelizejs.com/en/v3/docs/getting-started/
+   *             http://docs.sequelizejs.com/en/1.7.0/docs/usage/
+   *             http://docs.sequelizejs.com/en/1.7.0/articles/express/
+   *
+   * @param conf {Object}, Sequelize configuration.
+   *
+   * @return {Object} Database object that contains the following structure
+   */
+  initialize : function (conf) {
+
+    // Initialize the new sequelize instance
+    this.sequelize = new Sequelize(conf.database, conf.username, conf.password, {
       dialect : conf.dialect,
       port    : conf.port,
       host    : conf.host,
@@ -29,14 +43,22 @@ let db = {
       }
     });
 
+    // Lookup for all the models in the current directory
     fs.readdirSync(__dirname)
-      .filter(function(file) {
+      .filter((file) => {
         return (file.indexOf(".js",file.length - 3) !== -1) && (file !== "index.js");
       })
-      .forEach(function(file) {
-        var model = me.sequelize.import('./' + file);
-        me[model.name] = model;
+      .forEach((file) => {
+        // Import the sequelize model
+        let model = this.sequelize.import('./' + file);
+        // Save the reference
+        this[model.name] = model;
       });
+
+    Object.keys(this).forEach((modelName) => {
+      if ("associate" in this[modelName])
+        this[modelName].associate(this);
+    });
   }
 }
 
