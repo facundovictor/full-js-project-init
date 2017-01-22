47fb6c4e5195999decf4fc341fb233f8226c99fb
Facundo Victor <facundovt@gmail.com>
2017-01-22 11:45:54 -0300

    Add route provider

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,2 +1,14 @@
 
-console.log('something');
+var app = angular.module('publicApp', [
+  'ngRoute'
+]).config(function ($routeProvider) {
+  $routeProvider
+    .when('/clients', {
+      templateUrl  : 'components/client/clientView.html',
+      controller   : 'clientController',
+      controllerAs : 'client'
+    })
+    .otherwise({
+      redirectTo : '/clients'
+    });
+});
