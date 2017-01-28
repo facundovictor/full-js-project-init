80e5cb5969e6de738d7dd253d4c2b9b28ffa6fa5
Facundo Victor <facundovt@gmail.com>
2017-01-28 16:47:47 -0300

    Add provider form section style

---
+++ client/src/app/components/provider/providerDirective.js
@@ -0,0 +1,14 @@
+/*
+ * Author : Facundo Victor <facundovt@gmail.com>
+ *
+ */
+
+app.directive('providerForm', function(){
+  return {
+    restrict    : 'E',
+    templateUrl : 'components/provider/providerFormView.html',
+    transclude  : true,
+    replace     : true,
+    scope       : false
+  };
+});
