a00de3d1a8d80a91f63bc568da0a53608a821e88
Facundo Victor <facundovt@gmail.com>
2017-01-28 17:15:39 -0300

    Add provider service

---
+++ client/src/app/components/provider/providerService.js
@@ -0,0 +1,52 @@
+/*
+ * Author : Facundo Victor <facundovt@gmail.com>
+ *
+ * Communication service for managing the entity 'provider'.
+ */
+
+'use strict';
+
+class ProviderService {
+  constructor ($http, appConfig) {
+    this.$http = $http;
+    this.appConfig = appConfig;
+  }
+
+  /*
+   * Recover all the providers by requesting 'GET /api/provider'
+   *
+   * @return {Promise}, The promise of the list of providers.
+   */
+  getAllProviders () {
+    return this.$http
+      .get(`${this.appConfig.apiUrl}/api/provider`)
+      .then( response => {
+        if (response.status === 200)
+          return response.data;
+        throw new Error(`Reponse with status ${response.status}`);
+      });
+  }
+
+  /*
+   * Deletes the provider identified by the id. 'DELETE /api/provider/{id}'
+   *
+   * @param id {Number}, the provider's identifier.
+   *
+   * @return {Promise}, The promise of the delete action.
+   */
+  deleteProvider (id) {
+    return this.$http
+      .delete(`${this.appConfig.apiUrl}/api/provider/${id}`)
+      .then( response => {
+        if (response.status === 204)
+          return response.data;
+        throw new Error(`Reponse with status ${response.status}`);
+      });
+  }
+}
+
+app.service('providerService', [
+  '$http',
+  'appConfig',
+  ProviderService
+]);
