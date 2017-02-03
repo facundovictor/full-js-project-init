aa450c9f7d3e71da3b3466be8631d1897e36e827
Facundo Victor <facundovt@gmail.com>
2017-02-03 12:22:26 -0300

    Remove unncessary transclude property

--- client/src/app/components/provider/providerDirective.js
+++ client/src/app/components/provider/providerDirective.js
@@ -7,7 +7,6 @@ app.directive('providerForm', function(){
   return {
     restrict     : 'E',
     templateUrl  : 'components/provider/providerFormView.html',
-    transclude   : true,
     replace      : true,
     scope        : false,
     controller   : 'providerController',
