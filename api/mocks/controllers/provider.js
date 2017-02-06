6c807da5e0387f723c852cad058a453516eea3b7
Facundo Victor <facundovt@gmail.com>
2017-02-05 21:40:00 -0300

    Add delete method mocks

---
+++ api/mocks/controllers/provider.js
@@ -0,0 +1,34 @@
+/*
+ * Author : Facundo Victor <facundovt@gmail.com>
+ *
+ * References:
+ *     https://github.com/theganyo/swagger-node-runner/issues/79
+ */
+
+'use strict';
+
+// Exposed methods
+module.exports = {
+  // getAllProviders,
+  // addProvider,
+  // updateProvider,
+  deleteProvider
+};
+
+/*
+ * DELETE /provider/{id}
+ *
+ * Remove the provider specified by the 'id' parameter.
+ *
+ * This needs to be mocked, because it returns a response with no body, and
+ * this is not supported by the auto-mock provided by swagger.
+ *
+ * Reference:
+ *        https://github.com/theganyo/swagger-node-runner/issues/79
+ */
+function deleteProvider (req, res) {
+  if (req.swagger.params.id.value < 100)
+    res.sendStatus(204);
+  else
+    res.sendStatus(404);
+}
